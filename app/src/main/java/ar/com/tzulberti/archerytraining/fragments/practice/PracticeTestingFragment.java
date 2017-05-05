6fa11c62c1aecb4ef242009a1d8aa19d9af09ef5
Tomas Zulberti <tzulberti@gmail.com>
2017-05-04 21:22:34 -0300

    Improved clicks on image

--- app/src/main/java/ar/com/tzulberti/archerytraining/fragments/practice/PracticeTestingFragment.java
+++ app/src/main/java/ar/com/tzulberti/archerytraining/fragments/practice/PracticeTestingFragment.java
@@ -20,14 +20,20 @@ import android.widget.TextView;
 
 import com.github.chrisbanes.photoview.PhotoView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ar.com.tzulberti.archerytraining.R;
 import ar.com.tzulberti.archerytraining.fragments.BaseClickableFragment;
+import ar.com.tzulberti.archerytraining.model.Coordinate;
 
 /**
  * Created by tzulberti on 4/25/17.
  */
 
 public class PracticeTestingFragment extends BaseClickableFragment implements View.OnTouchListener, View.OnLongClickListener {
+
+    private static final int Y_PADDING = -80;
 
     private ImageView targetImageView;
 
@@ -38,12 +44,27 @@ public class PracticeTestingFragment extends BaseClickableFragment implements Vi
 
     private static final int IMAGE_WIDTH = 512;
 
+    private Bitmap imageBitmap;
+    private List<Coordinate> coordinates;
+    private Paint currentImpactPaint;
+    private Paint finalImpactPaint;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.practice_testing, container, false);
         this.targetImageView = (ImageView) view.findViewById(R.id.photo_view);
         this.targetImageView.setOnTouchListener(this);
         this.targetImageView.setOnLongClickListener(this);
+
+        this.coordinates = new ArrayList<Coordinate>();
+        this.currentImpactPaint = new Paint();
+        this.currentImpactPaint.setAntiAlias(true);
+        this.currentImpactPaint.setColor(Color.MAGENTA);
+
+        this.finalImpactPaint = new Paint();
+        this.finalImpactPaint.setAntiAlias(true);
+        this.finalImpactPaint.setColor(Color.LTGRAY);
+
 
         return view;
     }
@@ -64,6 +85,19 @@ public class PracticeTestingFragment extends BaseClickableFragment implements Vi
             this.imageScale = Math.min(this.targetImageView.getWidth(), this.targetImageView.getHeight()) / IMAGE_WIDTH;
         }
 
+        if (this.imageBitmap == null) {
+            BitmapFactory.Options myOptions = new BitmapFactory.Options();
+            myOptions.inScaled = false;
+            myOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;// important
+
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.complete_archery_target, myOptions);
+            Paint paint = new Paint();
+            paint.setAntiAlias(true);
+            paint.setColor(Color.BLUE);
+
+            this.imageBitmap = Bitmap.createBitmap(bitmap);
+        }
+
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
             // when the user clicks on a point in the mouse
             //System.out.println(String.format("Start %s - %s", event.getX(), event.getY()));
@@ -76,81 +110,13 @@ public class PracticeTestingFragment extends BaseClickableFragment implements Vi
         } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
             // when the user starts moving
             this.currentScore(event.getX(), event.getY());
+            Coordinate currentImpact = new Coordinate(event.getX() / this.imageScale, event.getY() / this.imageScale);
+            this.addTargetImpact(currentImpact.x, currentImpact.y, false);
+
         } else if (event.getAction() == MotionEvent.ACTION_UP){
-            // TODOS estos valores son 0
-            //System.err.println(String.format("EMPEZANDO: %s - %s - %s", this.targetImageView.getX(), this.targetImageView.getLeft(), this.targetImageView.getTranslationX()));
-            // Por alguna razon el resultado de esto es 1080 - 1536
-            //System.err.println(String.format("EMPEZANDO2: %s - %s", this.targetImageView.getWidth(), this.targetImageView.getHeight()));
-            //System.err.println(String.format("EMPEZANDO3: %s - %s", this.targetImageView.getMaxHeight(), this.targetImageView.getWidth()));
-            //System.err.println(String.format("EMPEZANDO4: %s - %s", this.targetImageView.getMeasuredHeight(), this.targetImageView.getMeasuredWidth()));
-            System.err.println(String.format("EMPEZANDO5: %s - %s", this.targetImageView.getScaleX(), this.targetImageView.getScaleY()));
-
-            BitmapFactory.Options myOptions = new BitmapFactory.Options();
-            //myOptions.inDither = true;
-            myOptions.inScaled = false;
-            myOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;// important
-            //myOptions.inPurgeable = true;
-
-            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.complete_archery_target, myOptions);
-            Paint paint = new Paint();
-            paint.setAntiAlias(true);
-            paint.setColor(Color.BLUE);
-
-            Bitmap workingBitmap = Bitmap.createBitmap(bitmap);
-            Bitmap mutableBitmap = workingBitmap.copy(Bitmap.Config.ARGB_8888, true);
-
-            Canvas canvas = new Canvas(mutableBitmap);
-            //canvas.drawCircle(0, 0, 25, paint);
-            /*
-            for (int i = 0; i < 10; i++) {
-                canvas.drawCircle(i * 100, i * 100, 25, paint);
-            }
-            */
-            canvas.drawCircle(event.getX() / this.imageScale, event.getY() / this.imageScale, 25, paint);
-            /*
-            canvas.drawCircle(this.targetImageView.getWidth(), 0, 25, paint);
-            canvas.drawCircle(this.targetImageView.getHeight(), 0, 25, paint);
-            canvas.drawCircle(this.targetImageView.getHeight(), this.targetImageView.getWidth(), 25, paint);
-            */
-
-            //canvas.drawCircle(this.targetImageView.getTop(), this.targetImageView.getLeft(), 25, paint);
-            /*
-            canvas.drawCircle(200, 100, 25, paint);
-            canvas.drawCircle(300, 200, 25, paint);
-            canvas.drawCircle(400, 300, 25, paint);
-            canvas.drawCircle(500, 400, 25, paint);
-            canvas.drawCircle(600, 500, 25, paint);
-            canvas.drawCircle(700, 600, 25, paint);
-            canvas.drawCircle(800, 700, 25, paint);
-            canvas.drawCircle(900, 800, 25, paint);
-            */
-            //canvas.drawCircle(event.getX() - this.targetImageView.getX(), event.getY() - this.targetImageView.getY(), 25, paint);
-            //System.err.println(String.format("END %s - %s - %s - %s", event.getX(), event.getY(), event.getX() - this.targetImageView.getLeft(), event.getY() - this.targetImageView.getTop()));
-            /**
-            canvas.drawCircle(this.targetImageView.getTop(), this.targetImageView.getRight(), 25, paint);
-            canvas.drawCircle(this.targetImageView.getBottom(), this.targetImageView.getRight(), 25, paint);
-            canvas.drawCircle(this.targetImageView.getBottom(), this.targetImageView.getLeft(), 25, paint);
-            */
-
-            this.targetImageView.setAdjustViewBounds(true);
-            this.targetImageView.setImageBitmap(mutableBitmap);
-
-
-            /*
-            // when the user finish moving
-            Bitmap bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
-            Canvas canvas = new Canvas(bitmap);
-            Paint paint = new Paint();
-            paint.setColor(Color.BLACK);
-            canvas.drawCircle(event.getX(), event.getY(), 80, paint);
-
-            Drawable asd = this.targetImageView.getDrawable();
-            ShapeDrawable sd = new ShapeDrawable(new OvalShape());
-            asd.draw(sd);
-            //this.targetImageView.setImageBitmap(bitmap);
-            this.targetImageView.draw(canvas);
-            */
-            //System.out.println(String.format("END %s - %s", event.getX(), event.getY()));
+            Coordinate currentImpact = new Coordinate(event.getX() / this.imageScale, event.getY() / this.imageScale);
+            this.coordinates.add(currentImpact);
+            this.addTargetImpact(currentImpact.x, currentImpact.y, true);
         } else {
             //System.out.println(String.format("%s - %s - %s", event.getAction(), event.getX(), event.getY()));
         }
@@ -161,6 +127,25 @@ public class PracticeTestingFragment extends BaseClickableFragment implements Vi
         double distance = Math.sqrt(Math.pow(x - this.targetCenterX, 2) + Math.pow(y - this.targetCenterY, 2));
         //System.out.println(String.format("Current score: %s", 10 - Math.floor(distance / this.pointWidth)));
     }
+
+    private void addTargetImpact(float x, float y, boolean isFinal) {
+        Bitmap mutableBitmap = this.imageBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        Paint paint = this.finalImpactPaint;
+        if (! isFinal) {
+            paint = this.currentImpactPaint;
+        } else {
+            this.imageBitmap = mutableBitmap;
+        }
+        Canvas canvas = new Canvas(mutableBitmap);
+        canvas.drawCircle(x, y + Y_PADDING, 10, paint);
+
+        this.targetImageView.setAdjustViewBounds(true);
+        this.targetImageView.setImageBitmap(mutableBitmap);
+    }
+
+
+
+
 
     @Override
     public boolean onLongClick(View v) {
---
+++ app/src/main/java/ar/com/tzulberti/archerytraining/model/Coordinate.java
@@ -0,0 +1,16 @@
+package ar.com.tzulberti.archerytraining.model;
+
+/**
+ * Created by tzulberti on 5/4/17.
+ */
+
+public class Coordinate {
+
+    public float x;
+    public float y;
+
+    public Coordinate(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+}
