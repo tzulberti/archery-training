dff2b5be091477b51d60564fe9439974d0baa070
Tomas Zulberti <tzulberti@gmail.com>
2017-05-16 23:18:49 -0300

    Added missing file

---
+++ app/src/main/java/ar/com/tzulberti/archerytraining/consts/TournamentSerieConsts.java
@@ -0,0 +1,15 @@
+package ar.com.tzulberti.archerytraining.consts;
+
+/**
+ * Created by tzulberti on 5/16/17.
+ */
+
+public class TournamentSerieConsts {
+
+    public static final String TABLE_NAME = "tournament_series";
+
+    public static final String ID_COLUMN_NAME = "id";
+    public static final String TOURNAMENT_ID_COLUMN_NAME = "tournament_id";
+    public static final String SERIE_INDEX_COLUMN_NAME = "serie_index";
+    public static final String TOTAL_SCORE_COLUMN_NAME = "total_score";
+}
