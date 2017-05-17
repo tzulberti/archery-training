8ebffcf51abb87531bc21369262cbb47440d8104
Tomas Zulberti <tzulberti@gmail.com>
2017-05-16 22:44:08 -0300

    Created the scoring tables information

--- app/src/main/java/ar/com/tzulberti/archerytraining/consts/PracticeConsts.java
+++ app/src/main/java/ar/com/tzulberti/archerytraining/consts/PracticeConsts.java
@@ -6,13 +6,14 @@ package ar.com.tzulberti.archerytraining.consts;
 
 public class PracticeConsts {
 
-    public static final String TABLE_NAME = "pracitce_information";
+    public static final String TABLE_NAME = "practice_information";
 
     public static final String ID_COLUMN_NAME = "id";
     public static final String NAME_COLUMN_NAME = "name";
     public static final String DATETIME_COLUMN_NAME = "datetime";
     public static final String DISTANCE_COLUMN_NAME = "distance";
     public static final String TARGET_SIZE_COLUMN_NAME = "target_size";
-    public static final String OUTDOOR_COLUMN_NAME = "is_outdoor";
+    public static final String IS_OUTDOOR_COLUMN_NAME = "is_outdoor";
+    public static final String IS_TOURNAMENT_DATA_COLUMN_NAME = "is_tournament_data";
 
 }
--- app/src/main/java/ar/com/tzulberti/archerytraining/helper/DatabaseHelper.java
+++ app/src/main/java/ar/com/tzulberti/archerytraining/helper/DatabaseHelper.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import ar.com.tzulberti.archerytraining.consts.PracticeConsts;
+import ar.com.tzulberti.archerytraining.consts.ScoringInformationConsts;
 import ar.com.tzulberti.archerytraining.consts.SerieInformationConsts;
 
 /**
@@ -14,7 +16,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "archery_training.db";
 
 
-    protected static final int DATABASE_VERSION = 2;
+    protected static final int DATABASE_VERSION = 3;
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -27,12 +29,46 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 SerieInformationConsts.TABLE_NAME, SerieInformationConsts.ID_COLUMN_NAME, SerieInformationConsts.DATETIME_COLUMN_NAME,
                 SerieInformationConsts.DISTANCE_COLUMN_NAME, SerieInformationConsts.ARROWS_AMOUNT_COLUMN_NAME
         ));
+        this.createPracticeTable(db);
+        this.createScoringInformationTable(db);
+    }
+
+    private void createPracticeTable(SQLiteDatabase db) {
+        db.execSQL(
+                "CREATE TABLE " + PracticeConsts.TABLE_NAME + "( " +
+                        PracticeConsts.ID_COLUMN_NAME + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        PracticeConsts.NAME_COLUMN_NAME + " TEXT NOT NULL, " +
+                        PracticeConsts.DATETIME_COLUMN_NAME + " LONG NOT NULL, " +
+                        PracticeConsts.DISTANCE_COLUMN_NAME + " INTEGER NOT NULL, " +
+                        PracticeConsts.IS_TOURNAMENT_DATA_COLUMN_NAME + " INTEGER NOT NULL, " +
+                        PracticeConsts.IS_OUTDOOR_COLUMN_NAME + " INTEGER NOT NULL, " +
+                        PracticeConsts.TARGET_SIZE_COLUMN_NAME + " INTEGER NOT NULL " +
+                        ");"
+        );
+    }
+
+    private void createScoringInformationTable(SQLiteDatabase db) {
+        db.execSQL(
+                "CREATE TABLE " + ScoringInformationConsts.TABLE_NAME + " (" +
+                        ScoringInformationConsts.ID_COLUMN_NAME + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        ScoringInformationConsts.PRACTICE_ID_COLUMN_NAME + " INTEGER NOT NULL, " +
+                        ScoringInformationConsts.SERIE_INDEX_COLUMN_NAME + " INTEGER NOT NULL, " +
+                        ScoringInformationConsts.SCORE_COLUMN_NAME + " INTEGER NOT NULL, " +
+                        ScoringInformationConsts.X_POSITION_COLUMN_NAME + " INTEGER NOT NULL, " +
+                        ScoringInformationConsts.Y_POSITION_COLUMN_NAME + " INTEGER NOT NULL, " +
+                        "FOREIGN KEY (" + ScoringInformationConsts.PRACTICE_ID_COLUMN_NAME + ") REFERENCES " + PracticeConsts.TABLE_NAME + " ( " +  PracticeConsts.ID_COLUMN_NAME + " )" +
+                        ");"
+        );
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL(String.format("DROP TABLE IF EXISTS %s", SerieInformationConsts.TABLE_NAME));
-        this.onCreate(db);
+        if (oldVersion == 2) {
+            this.createPracticeTable(db);
+            this.createScoringInformationTable(db);
+        } else {
+            throw new RuntimeException("Unexpected oldVersion: " + String.valueOf(oldVersion));
+        }
     }
 
 
