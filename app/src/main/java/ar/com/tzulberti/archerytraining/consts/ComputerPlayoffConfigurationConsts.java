2326ba201081213c990750e498cd57262ef46d0a
Tomas Zulberti <tzulberti@gmail.com>
2017-06-03 09:40:57 -0300

    ISSUE-76: Added playoff table constants

---
+++ app/src/main/java/ar/com/tzulberti/archerytraining/consts/ComputerPlayoffConfigurationConsts.java
@@ -0,0 +1,15 @@
+package ar.com.tzulberti.archerytraining.consts;
+
+/**
+ * Created by tzulberti on 6/2/17.
+ */
+
+public class ComputerPlayoffConfigurationConsts {
+
+    public static final String TABLE_NAME = "computer_playoff_configuration";
+
+    public static final String ID_COLUMN_NAME = "id";
+    public static final String PLAYOFF_ID = "playoff_id";
+    public static final String MIN_SCORE = "min_socre";
+    public static final String MAX_SCORE = "max_score";
+}
---
+++ app/src/main/java/ar/com/tzulberti/archerytraining/consts/PlayoffSerieConsts.java
@@ -0,0 +1,16 @@
+package ar.com.tzulberti.archerytraining.consts;
+
+/**
+ * Created by tzulberti on 6/2/17.
+ */
+
+public class PlayoffSerieConsts {
+
+    public static final String TABLE_NAME = "playoff_serie";
+
+    public static final String ID_COLUMN_NAME = "id";
+    public static final String PLAYOFF_ID_COLUMN_NAME = "playoff_id";
+    public static final String INDEX_COLUMN_NAME = "index";
+    public static final String OPPONENT_TOTAL_SCORE_COLUMN_NAME_COLUMN_NAME = "opponent_total_score";
+    public static final String USER_TOTAL_SCORE_COLUMN_NAME = "user_total_score";
+}
