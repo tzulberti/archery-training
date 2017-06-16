4a65e88d741a949d8078769eb4289d4f5bea4be1
Tomas Zulberti <tzulberti@gmail.com>
2017-06-16 20:49:54 -0300

    ISSUE-126: Changed so all epochs are in seconds

--- app/src/main/java/ar/com/tzulberti/archerytraining/helper/DatetimeHelper.java
+++ app/src/main/java/ar/com/tzulberti/archerytraining/helper/DatetimeHelper.java
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 /**
  * Created by tzulberti on 4/19/17.
@@ -25,7 +26,7 @@ public class DatetimeHelper {
         date.set(Calendar.MINUTE, 0);
         date.set(Calendar.SECOND, 0);
         date.set(Calendar.MILLISECOND, 0);
-        return date.getTimeInMillis();
+        return date.getTimeInMillis() / 1000;
     }
 
     public static long getTomorrowZeroHours() {
@@ -37,14 +38,39 @@ public class DatetimeHelper {
         date.set(Calendar.MILLISECOND, 0);
         // next day
         date.add(Calendar.DAY_OF_MONTH, 1);
-        return date.getTimeInMillis();
+        return date.getTimeInMillis() / 1000;
+    }
+
+    public static long getFirstDateOfMonth() {
+        Calendar date = new GregorianCalendar();
+        // reset hour, minutes, seconds and millis
+        date.set(Calendar.HOUR_OF_DAY, 0);
+        date.set(Calendar.MINUTE, 0);
+        date.set(Calendar.SECOND, 0);
+        date.set(Calendar.MILLISECOND, 0);
+        date.set(Calendar.DAY_OF_MONTH, 1);
+        return date.getTimeInMillis() / 1000;
+    }
+
+    public static long getLastDateOfMonth() {
+        Calendar date = new GregorianCalendar();
+        // reset hour, minutes, seconds and millis
+        date.set(Calendar.HOUR_OF_DAY, 0);
+        date.set(Calendar.MINUTE, 0);
+        date.set(Calendar.SECOND, 0);
+        date.set(Calendar.MILLISECOND, 0);
+        date.set(Calendar.DATE, date.getActualMaximum(Calendar.DATE));
+        return date.getTimeInMillis() / 1000;
     }
 
     public static long getCurrentTime() {
-        return System.currentTimeMillis();
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTimeZone(TimeZone.getDefault());
+        return calendar.getTimeInMillis() / 1000;
     }
 
-    public static Date databaseValueToDate(long utcMilliseconds) {
-        return new Date(utcMilliseconds);
+    public static Date databaseValueToDate(long utcSeconds) {
+        return new Date(utcSeconds * 1000);
     }
+
 }
