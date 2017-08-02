5bd968c7a02ee83ebcde0536d65106634d15496c
Tomas Zulberti <tzulberti@gmail.com>
2017-08-01 22:48:49 -0300

    ISSUE-184: Rename the application

--- app/src/main/java/ar/com/tzulberti/archerytraining/database/DatabaseHelper.java
+++ app/src/main/java/ar/com/tzulberti/archerytraining/database/DatabaseHelper.java
@@ -7,16 +7,6 @@ import android.database.sqlite.SQLiteOpenHelper;
 import java.util.ArrayList;
 import java.util.List;
 
-import ar.com.tzulberti.archerytraining.database.migrations.DatabaseMigration20;
-import ar.com.tzulberti.archerytraining.database.migrations.DatabaseMigration21;
-import ar.com.tzulberti.archerytraining.database.migrations.DatabaseMigration23;
-import ar.com.tzulberti.archerytraining.database.migrations.DatabaseMigration27;
-import ar.com.tzulberti.archerytraining.database.migrations.DatabaseMigration29;
-import ar.com.tzulberti.archerytraining.database.migrations.DatabaseMigration4;
-import ar.com.tzulberti.archerytraining.database.migrations.DatabaseMigration5;
-import ar.com.tzulberti.archerytraining.database.migrations.DatabaseMigration6;
-import ar.com.tzulberti.archerytraining.database.migrations.DatabaseMigration7;
-import ar.com.tzulberti.archerytraining.database.migrations.DatabaseMigration10;
 import ar.com.tzulberti.archerytraining.database.migrations.IDatbasseMigration;
 import io.sentry.Sentry;
 
@@ -27,7 +17,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "archery_training.db";
 
-    protected static final int DATABASE_VERSION = 30;
+    protected static final int DATABASE_VERSION = 1;
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -53,16 +43,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // Get all the existing database migrations
         List<IDatbasseMigration> existingMigrations = new ArrayList<>();
+        /*
         existingMigrations.add(new DatabaseMigration4());
-        existingMigrations.add(new DatabaseMigration5());
-        existingMigrations.add(new DatabaseMigration6());
-        existingMigrations.add(new DatabaseMigration7());
-        existingMigrations.add(new DatabaseMigration10());
-        existingMigrations.add(new DatabaseMigration20());
-        existingMigrations.add(new DatabaseMigration21());
-        existingMigrations.add(new DatabaseMigration23());
-        existingMigrations.add(new DatabaseMigration27());
-        existingMigrations.add(new DatabaseMigration29());
+        */
 
         // make sure to disable any FK validation because the table might
         // create temporary table to drop the old schema table (SQLITE
